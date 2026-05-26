b028066d4cba87ec5b50980c6e0d4f6281e20b43
RAM MANOHAR REDDY <malirammanoharreddy@gmail.com>
2026-05-26 13:27:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//your JS code here. If required.
+//your JS code here. If 
+const line = document.getElementById("line");
+let deg = 0;
+setInterval(function(){
+			line.style.transform = `rotate(${deg}deg)`;
+			deg+=2;
+		},20)
